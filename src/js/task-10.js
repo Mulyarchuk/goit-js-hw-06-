3a47cc2b505439cf51b41dbf0c7b6afa463310de
Mulyarchuk Elena <ml.mulyarchuk@gmail.com>
2022-12-03 22:04:25 +0300

    hw1

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,25 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const input = document.querySelector(`#controls`);
+const create = document.querySelector(`[data-create]`);
+const destroy = document.querySelector(`[data-destroy]`);
+const boxes = document.querySelector(`#boxes`);
+const amount = document.querySelector(`input`)
+console.log(amount)
+
+const divEl= document.createElement (`div`);
+divEl.style.width = 30 + `px`;
+divEl.style.height = 30 + `px`;
+divEl.style.backgroundColor = getRandomHexColor();
+
+
+
+const createBoxes = (amount) => {
+  boxes.append(divEl);
+
+
+console.log (divEl)
+
+}
+create.addEventListener(`click`, createBoxes);
